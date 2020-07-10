d1ec92d643c0ff83ca821357c6e3f43bc85802af
ZaStack <stackzpersonal@gmail.com>
2020-07-10 09:51:51 -0700

    Completes ex2.py

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -9,6 +9,16 @@ def reconstruct_trip(tickets, length):
     """
     YOUR CODE HERE
     """
-    # Your code here
+    # Initialize empty list and then fill a dictionary with each tickets source and destination.
+    route = [None] * length
+    locations = dict()
+    for ticket in tickets:
+        locations[ticket.source] = ticket.destination
+    # Set the first destination using the ticket with None source
+    next = locations["NONE"]
+    # Add source to list, move to destination, repeat
+    for i in range(0, length):
+        route[i] = next
+        next = locations[next]
 
     return route
